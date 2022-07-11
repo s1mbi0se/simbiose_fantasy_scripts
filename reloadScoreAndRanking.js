ada5a1bdebe73ef3832ec5ddbafe4ae8f931fa30
s1mbi0se <hello@simbioseventures.com>
2022-07-11 11:26:43 +0000

    See https://github.com/s1mbi0se/fantasy-scripts/commit/33ca65690afb12aa4899f61527fbc975bfc36e90 from refs/pull/5/merge

--- reloadScoreAndRanking.js
+++ reloadScoreAndRanking.js
@@ -1,1 +1,1 @@
-setTimeout((function(){location.reload()}),2e4);/*! Mon Jul 11 2022 11:21:59 GMT+0000 (Coordinated Universal Time) !*/
+setTimeout((function(){location.reload()}),2e4);/*! Mon Jul 11 2022 11:26:39 GMT+0000 (Coordinated Universal Time) !*/
