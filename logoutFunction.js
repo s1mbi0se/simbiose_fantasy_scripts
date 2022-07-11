72e7266ad1423238e3dc167efce310fd4b09efe6
s1mbi0se <hello@simbioseventures.com>
2022-07-11 10:56:02 +0000

    See https://github.com/s1mbi0se/fantasy-scripts/commit/49a223d7ec8b2b626810a4752623423ee0ad8aa8 from refs/heads/master

--- logoutFunction.js
+++ logoutFunction.js
@@ -1,1 +1,1 @@
-components.modalSair.onClick((()=>{logOut()}));/*! Mon Jul 11 2022 10:44:51 GMT+0000 (Coordinated Universal Time) !*/
+components.modalSair.onClick((()=>{logOut()}));/*! Mon Jul 11 2022 10:55:58 GMT+0000 (Coordinated Universal Time) !*/
